92549beb613c8aa751437f11560212105b0451ff
hgkim <makga87@naver.com>
2021-05-09 21:33:42 +0900

    # 가장 짧은 거리 문자열

---
+++ algorithm/src/string/ShortestCharStreet.java
@@ -0,0 +1,46 @@
+package string;
+
+import java.util.Scanner;
+
+public class ShortestCharStreet {
+
+    public int[] solution(String str, char t) {
+
+        int[] answer = new int[str.length()];
+
+        int p = 1000;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == t) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+
+        p = 1000;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == t) p = 0;
+            else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        ShortestCharStreet t = new ShortestCharStreet();
+        Scanner kb = new Scanner(System.in);
+        String input1 = kb.next();
+        char c = kb.next().charAt(0);
+        for (int x : t.solution(input1, c)) {
+            System.out.print(x + " ");
+        }
+    }
+}
