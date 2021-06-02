80cdcbecdc08c6d8255821b6b72a6041c7083528
hgkim <makga87@naver.com>
2021-06-02 23:00:10 +0900

    # 병합정렬

---
+++ algorithm/src/main/java/sort/MergeSort.java
@@ -0,0 +1,53 @@
+package sort;
+
+public class MergeSort {
+
+
+    private static void mergeSort(int[] arr) {
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, tmp, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(int[] arr, int[] tmp, int start, int end) {
+
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, tmp, start, mid);
+            mergeSort(arr, tmp, mid + 1, end);
+            merge(arr, tmp, start, mid, end);
+        }
+    }
+
+    private static void merge(int[] arr, int[] tmp, int start, int mid, int end) {
+
+        for (int i = start; i <= end; i++) {
+            tmp[i] = arr[i];
+        }
+        int part1 = start;
+        int part2 = mid + 1;
+        int index = start;
+        while (part1 <= mid && part2 <= end) {
+            if (tmp[part1] <= tmp[part2]) {
+                arr[index] = tmp[part1];
+                part1++;
+            } else {
+                arr[index] = tmp[part2];
+                part2++;
+            }
+            index++;
+        }
+        for (int i = 0; i <= mid - part1; i++) {
+            arr[index + 1] = tmp[part1 + i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 1, 2, 4, 6, 7, 0, 9};
+
+        mergeSort(arr);
+
+        for(int n : arr){
+            System.out.print(n + " ");
+        }
+    }
+}
