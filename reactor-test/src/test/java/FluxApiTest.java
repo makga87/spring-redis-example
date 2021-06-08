cd8ecd62414b0af96d02ea079ff2ec04401f1785
hgkim <makga87@naver.com>
2021-06-08 23:02:52 +0900

    # java reactor Flux test. 아직까진 비동기인지는 감이 안잡힌다.

--- reactor-test/src/test/java/FluxApiTest.java
+++ reactor-test/src/test/java/FluxApiTest.java
@@ -1,17 +1,38 @@
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.Flushable;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxApiTest {
 
     @Test
-    void 심플한_FLUX를_테스트해본다(){
+    void 심플한_FLUX를_테스트해본다() {
         Flux.just(1, 2, 3, 4, 5)
                 .log()
-                .subscribe(data->System.out.println(data));
+                .subscribe(data -> System.out.println(data));
+    }
+
+    @Test
+    void ITERABLE_객체로부터_FLUX를_생성해본다() {
+        List<String> iterable = Arrays.asList("a", "b", "c", "d");
+        Flux<String> flux = Flux.fromIterable(iterable);
+
+        flux
+            .log()
+            .subscribe(data -> System.out.println(data));
+
+    }
+
+    @Test
+    void 심플한_MONO를_테스트해본다() {
+        Mono.just(1)
+                .log()
+                .subscribe(data -> System.out.println(data));
 
     }
 }
