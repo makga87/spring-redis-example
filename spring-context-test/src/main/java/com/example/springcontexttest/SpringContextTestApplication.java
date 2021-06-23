4899dec5d1d6beacdfb9b7f71c45cfecf18838ca
hgkim8704 <hgkim8704@gmail.com>
2021-06-23 22:56:38 +0900

    # spring bean의 사이클과 동작등을 테스트하기 위한 예제 소스

---
+++ spring-context-test/src/main/java/com/example/springcontexttest/SpringContextTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcontexttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContextTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringContextTestApplication.class, args);
+    }
+
+}
---
+++ spring-context-test/src/test/java/com/example/springcontexttest/SpringContextTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcontexttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContextTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
