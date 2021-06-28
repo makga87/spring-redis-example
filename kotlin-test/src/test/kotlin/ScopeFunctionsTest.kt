47712a4dac1762d52ebe96d53f4f0794bd2213a6
hgkim <makga87@naver.com>
2021-06-28 23:40:00 +0900

    #kotlin test ScopeFunctionsTest.kt with, applytest

--- kotlin-test/src/test/kotlin/ScopeFunctionsTest.kt
+++ kotlin-test/src/test/kotlin/ScopeFunctionsTest.kt
@@ -18,12 +18,45 @@ internal class ScopeFunctionsTest {
      * let과 비슷하게 쓰이지만, 내부적으로 this란 개념이 존재
      */
     @Test
-    fun runTest1(){
+    fun runTest1() {
         val string = "abcdefg"
         string.run {
             println(isEmpty())
             println(this) // let과 다르게 상수값이 나온다.
             println(length)
         }
+    }
+
+    @Test
+    fun withTest1() {
+        val withTest = WithTest();
+        println("println : ${withTest.host}${withTest.uri}")
+
+        with(withTest) {
+            println("with funtion: ${withTest.host}${withTest.uri}")
+            println("with funtion: $host$uri")
+        }
+    }
+
+    class WithTest() {
+        val host = "http://localhost:8080"
+        val uri = "/hello"
+    }
+
+    @Test
+    fun applyTest1() {
+        val tom = Person()
+
+        val whoIsTom = tom.apply {
+            name = "Tom"
+            age = 35
+            desc = "Web developer"
+        }
+    }
+
+    class Person() {
+        var name: String = ""
+        var age: Int = 0
+        var desc: String =""
     }
 }
