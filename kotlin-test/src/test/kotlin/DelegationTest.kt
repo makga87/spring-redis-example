afcc61b90870986cf54f1729f691850b3b007932
hgkim <makga87@naver.com>
2021-07-01 23:05:31 +0900

    #kotlin test DelegationTest.kt delegate properties test

--- kotlin-test/src/test/kotlin/DelegationTest.kt
+++ kotlin-test/src/test/kotlin/DelegationTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions.*
+import kotlin.reflect.KProperty
 import kotlin.test.Test
 
 internal class DelegationTest {
@@ -19,6 +20,7 @@ internal class DelegationTest {
 
     class ElvisPresley(n: String) : SoundBehavior by RockAndRollBehavior(n)
 
+
     @Test
     fun delegateTest1() {
         val tomAraya = TomAraya("EveryBody")
@@ -26,6 +28,64 @@ internal class DelegationTest {
 
         val elvisPresley = ElvisPresley("HelloWorld")
         elvisPresley.makeSound()
+    }
+
+
+    class Example {
+        var p: String by Delegate()
+
+        override fun toString() = "Example String"
+    }
+
+    class Delegate() {
+        operator fun getValue(thisRef: Any?, prop: KProperty<*>): String {
+            return "$thisRef, thank you for delegating '${prop.name} to me!"
+        }
+
+        operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: String) {
+            println("$value has been assigned to ${prop.name} in $thisRef")
+        }
+    }
+
+    @Test
+    fun delegateTest2() {
+        val example = Example()
+        println(example.p)
+        example.p = "NEW"
+    }
+
+    class LazySample {
+        init {
+            println("created!")
+        }
+
+        val lazyStr: String by lazy {
+            println("computed!")
+            "my lazy"
+        }
+    }
+
+    @Test
+    fun delegateTest3() {
+        val sample = LazySample()
+        println("lazyStr = ${sample.lazyStr}")
+        println(" = ${sample.lazyStr}")
+    }
+
+    class User(val map: Map<String, Any?>) {
+        val name: String by map
+        val age: Int by map
+    }
+
+    @Test
+    fun delegateTest4() {
+        val user = User(
+            mapOf(
+                "name" to "John Doe",
+                "age" to 25
+            )
+        )
+        println("name = ${user.name}, age = ${user.age}")
     }
 
 }
