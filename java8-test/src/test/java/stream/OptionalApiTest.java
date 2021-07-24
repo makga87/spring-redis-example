ba5bc0813e12823dfb51d45f7776caee625b5a20
hgkim <makga87@naver.com>
2021-07-24 22:27:39 +0900

    #Optional Api추가 테스트

--- java8-test/src/test/java/stream/OptionalApiTest.java
+++ java8-test/src/test/java/stream/OptionalApiTest.java
@@ -2,6 +2,7 @@ package stream;
 
 import org.junit.jupiter.api.Test;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,5 +18,23 @@ class OptionalApiTest {
 
         Optional<Employee> employee1 = Optional.ofNullable(employee);
         assertFalse(employee1.isPresent());
+    }
+
+    @Test
+    void Optional_값변환() {
+        Employee employee = new Employee("개발팀", "김갑돌", 50);
+        String name1 = Optional.ofNullable(employee)
+                .map(Employee::getName)
+                .orElse("없음");
+
+        System.out.println(name1);
+
+        String name2 = Optional.ofNullable(employee)
+                .map(Employee::getName)
+                .filter(name -> name.equals("김순돌"))
+                .orElse("없음");
+
+        System.out.println(name2);
+
     }
 }
