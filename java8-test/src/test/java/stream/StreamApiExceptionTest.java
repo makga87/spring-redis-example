ca8015aab9477f6eb206a59e176272b5cfe10f94
hyunggeun.kim <hyunggeun.kim@nhnsoft.com>
2021-09-27 12:22:30 +0900

    # For문 외부로 전달 시, 반복문이 멈춤을 테스트

--- java8-test/src/test/java/stream/StreamApiExceptionTest.java
+++ java8-test/src/test/java/stream/StreamApiExceptionTest.java
@@ -23,6 +23,17 @@ class StreamApiExceptionTest {
     }
 
     @Test
+    void For문안에서_Exception_발생시_외부로_던지면_반복문이_중단됨을_확인한다() throws Exception {
+        List<String> data = Arrays.asList("1", "2", "3", "4", "5");
+        for (String s : data) {
+            System.out.println(s);
+            if ("3".equals(s)) {
+                throw new Exception();
+            }
+        }
+    }
+
+    @Test
     void Stream안에서_Exception_발생시_캐치를_확인한다() {
 
         List<String> data = Arrays.asList("1", "2", "3", "4", "5");
@@ -38,4 +49,5 @@ class StreamApiExceptionTest {
             }
         });
     }
+
 }
