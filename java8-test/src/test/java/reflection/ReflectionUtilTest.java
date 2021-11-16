70ec7d892617f3c833e5a4c48d34a9268ad7b250
hyunggeun.kim <hyunggeun.kim@nhnsoft.com>
2021-11-16 18:27:16 +0900

    # reflection 테스트

---
+++ java8-test/src/test/java/reflection/ReflectionUtilTest.java
@@ -0,0 +1,14 @@
+package reflection;
+
+import jdk.internal.reflect.Reflection;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReflectionUtilTest {
+
+    @Test
+    void reflectionTest() throws Exception {
+    }
+
+}
