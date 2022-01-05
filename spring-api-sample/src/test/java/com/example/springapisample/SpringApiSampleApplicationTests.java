94cdbdf4dd5dc4563533c0eaf2fb35a761dd61d4
hyunggeun.kim <hyunggeun.kim@nhnsoft.com>
2022-01-06 06:54:05 +0900

    스프링 api 샘플용 리포지토리

---
+++ spring-api-sample/src/test/java/com/example/springapisample/SpringApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
